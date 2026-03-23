6cb18167798a07c58c6766ad1a2259cdd3470690
Kevin Manotas <kmanotas351@gmail.com>
2026-03-22 19:20:12 -0500

    Add PostgreSQL integration and CRUD with SQLAlchemy

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
-# Flask User API
+# Flask Users API
 
-Simple CRUD built with Flask
+A simple REST API built with Flask that implements a full CRUD system using PostgreSQL.
+
+## Features
+
+- Create users (POST)
+- Get all users (GET)
+- Get user by ID (GET)
+- Update users (PUT)
+- Delete users (DELETE)
+
 
 ## Endpoints
 
-- GET /users
-- GET /users/<id>
-- POST /users
-- PUT /users/<id>
-- DELETE /users/<id>
 
-# Tech stack 
+GET    | /users          Get all users           
+GET    | /users/<id>     Get user by ID          
+POST   | /users          Create a new user       
+PUT    | /users/<id>     Update user             
+DELETE | /users/<id>     Delete user             
+
+
+## Tech Stack
+
 - Python
 - Flask
+- PostgreSQL
+- SQLAlchemy
 
+---
 
+## Run locally
+
+```bash
+pip install -r requirements.txt
+python3 app.py
--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-users = [
-    {"id": 1, "nombre": "Kevin"},
-    {"id": 2, "nombre": "Atom"}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
@@ -13,36 +25,50 @@ def home():
 
 @app.route('/users')
 def get_users():
-    return jsonify(users)
+    users = User.query.all()
+
+    resultado = []
+
+    for user in users:
+        resultado.append({
+            "id": user.id,
+            "nombre": user.nombre
+        })
+    return jsonify(resultado)
 
 @app.route('/users', methods=['POST'])
 def create_users():
-    nuevo_usuario = request.get_json()
+    data = request.get_json()
 
-    if not nuevo_usuario:
+    if not data:
         return jsonify({"error": "no data"}), 400
 
-    if "nombre" not in nuevo_usuario:
+    if "nombre" not in data:
         return jsonify({"error": "Falta  nombre"}), 400 
 
-    nuevo_usuario ["id"] = len(users) + 1
-    users.append(nuevo_usuario)
-    return jsonify(nuevo_usuario), 200
+    nuevo_usuario = User(nombre=data["nombre"])
+
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+
+    return jsonify({
+        
+        "id": nuevo_usuario.id,
+        "nombre": nuevo_usuario.nombre
+    
+    }), 201
 
 @app.route('/users/<int:id>')
 def get_user(id):
-    for user in users:
-        if user ["id"] == id:
-            return jsonify(user)
-    return jsonify({"error": "Usario no encontrado"}), 404
+    user = User.query.get(id)
 
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_users(id):
-    for user in users:
-        if user["id"] == id:
-            users.remove(user)
-            return jsonify({"message": f"El user {id} ha sido eliminado"})
-    return jsonify({"error": "User no encontrado"}), 404
+    if not user:
+        return jsonify({"error": "Usario no encontrado"}), 404
+
+    return jsonify({
+        "id": user.id,
+        "nombre": user.nombre
+    })
 
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_users(id):
@@ -51,15 +77,32 @@ def update_users(id):
     if not data: 
         return jsonify({"error": "No data"}), 400
 
+    user = User.query.get(id)
 
-    for user in users:
-        if user["id"] == id:
-            user ["nombre"] = data["nombre"]
-            return jsonify({"Message": "Un nuevo usario ha sido actulizado"})
+    if not user:
+        return jsonify({"error": "Usario no encontrado"}), 404
 
-    return jsonify({"Error": "Usuario no encontrado"}), 404
+    user.nombre = data["nombre"]
 
+    db.commit()
+
+    return jsonify({
+        "id": user.id,
+        "nombre": user.nombre
+    })
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_users(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({"error": "Usario no encontrado"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"Message": f"El usario {id} ha sido eliminado"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
